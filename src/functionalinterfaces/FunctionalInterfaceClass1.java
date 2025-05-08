8f8bb33624a83245407d30e0ad76aa8711da40b9
https://github.com/PraveenaKanumalla538 <prknmlk@gmail.com>
2025-05-09 00:24:13 +0530

    functionalinterfaces

---
+++ src/functionalinterfaces/FunctionalInterfaceClass1.java
@@ -0,0 +1,8 @@
+package functionalinterfaces;
+@FunctionalInterface
+public interface FunctionalInterfaceClass1 {
+//public int sumOfNumbers(int a, int b);
+//public int multiply(int a, int b);
+//public int square(int x);
+	public double totalAmount (double principle, double interest);
+}
